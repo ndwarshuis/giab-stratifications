1dc78ebcb39a617b532bf9e80c3bb9b14d99c273
ndwarshuis <ndwar@yavin4.ch>
2023-03-30 19:04:36 -0400

    FIX actually filter xy feature

--- workflow/scripts/python/bedtools/xy/filter_sort_features.py
+++ workflow/scripts/python/bedtools/xy/filter_sort_features.py
@@ -1,7 +1,7 @@
 from typing import Any
 import common.config as cfg
-from common.bed import read_bed, filter_sort_bed, write_bed
 from pybedtools import BedTool as bt  # type: ignore
+from common.bed import read_bed, filter_sort_bed, write_bed
 
 
 def main(smk: Any, sconf: cfg.GiabStrats) -> None:
@@ -15,19 +15,17 @@ def main(smk: Any, sconf: cfg.GiabStrats) -> None:
     fs = sconf.refkey_to_strat(rk).xy.features
     bedfile = fs.x_bed if i is cfg.ChrIndex.CHRX else fs.y_bed
 
-    assert bedfile is not None, "this should not happen"
-
     level_col = bedfile.level_col
     conv = sconf.buildkey_to_chr_conversion(rk, bk, bedfile.chr_prefix)
 
     df = read_bed(smk.input["bed"], bedfile, [level_col])
     filtsort = filter_sort_bed(conv, df)
-    filtsort[level_col] = filtsort[level_col].str.contains(level)
+    filtsort = filtsort[filtsort[level_col].str.contains(level)].drop(
+        columns=[level_col]
+    )
     gapless = bt(smk.input["gapless"])
     nogaps = (
-        bt.from_dataframe(filtsort.drop(columns=[level_col]))
-        .intersect(b=gapless, sorted=True)
-        .to_dataframe()
+        bt.from_dataframe(filtsort).intersect(b=gapless, sorted=True).to_dataframe()
     )
     write_bed(smk.output[0], nogaps)
 
