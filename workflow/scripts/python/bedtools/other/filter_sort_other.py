e4cdccfe3927adefbb3088160dfb859bccb4f29d
ndwarshuis <ndwar@yavin4.ch>
2023-05-23 12:19:56 -0400

    FIX use correct params

--- workflow/scripts/python/bedtools/other/filter_sort_other.py
+++ workflow/scripts/python/bedtools/other/filter_sort_other.py
@@ -6,7 +6,9 @@ from common.bed import read_filter_sort_bed
 def main(smk: Any, sconf: cfg.GiabStrats) -> None:
     rk = cfg.RefKey(smk.wildcards["ref_key"])
     bk = cfg.BuildKey(smk.wildcards["build_key"])
-    ps = sconf.refkey_to_strat(rk).low_complexity.simreps.params
+    lk = cfg.OtherLevelKey(smk.wildcards["other_level_key"])
+    sk = cfg.OtherStratKey(smk.wildcards["other_strat_key"])
+    ps = sconf.otherkey_to_bed(rk, bk, lk, sk).params
     read_filter_sort_bed(sconf, smk.input[0], smk.output[0], ps, rk, bk)
 
 
