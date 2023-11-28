06b1db64d265223c1a61c6fe6ee427d030627d9d
ndwarshuis <ndwar@yavin4.ch>
2023-11-28 17:59:45 -0500

    WIP gross assertions and stuff

--- workflow/scripts/python/bedtools/low_complexity/filter_sort_trf.py
+++ workflow/scripts/python/bedtools/low_complexity/filter_sort_trf.py
@@ -1,8 +1,50 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, TypeVar, Callable
 import common.config as cfg
 
+X = TypeVar("X")
+Y = TypeVar("Y")
+
 # from common.bed import read_filter_sort_bed
+
+
+# TODO ewwwwwww
+def giant_switch_thingy(
+    inputs: list[X],
+    outputs: list[Y],
+    hap: cfg.Haplotype | None,
+    bd: cfg.HaploidBuildData | cfg.Diploid1BuildData | cfg.Diploid2BuildData,
+    hap_f: Callable[[X, Y, cfg.HaploidBuildData], None],
+    dip_to_dip_f: Callable[[X, Y, cfg.Diploid1BuildData], None],
+    hap_to_hap_f: Callable[[X, Y, cfg.Diploid2BuildData], None],
+    dip_to_hap_f: Callable[[X, Y, Y, cfg.Diploid2BuildData], None],
+    hap_to_dip_f: Callable[[X, X, Y, cfg.Diploid1BuildData], None],
+) -> None:
+    match (inputs, outputs):
+        case ([i], [o]):
+            # one haplotype for both bed and ref (no combine)
+            if isinstance(bd, cfg.HaploidBuildData) and hap is None:
+                hap_f(i, o, bd)
+            # one bed with both haps in it; one reference with both haps (no combine)
+            elif isinstance(bd, cfg.Diploid1BuildData) and hap is None:
+                dip_to_dip_f(i, o, bd)
+            # one bed and one ref for a single haplotype in a diploid reference
+            elif isinstance(bd, cfg.Diploid2BuildData) and hap is not None:
+                hap_to_hap_f(i, o, bd)
+        case ([i], [o0, o1]):
+            # one bed with both haps in it; two references for both haps (split)
+            if isinstance(bd, cfg.Diploid2BuildData) and hap is None:
+                dip_to_hap_f(i, o0, o1, bd)
+            else:
+                assert False, "this should not happen"
+        case ([i0, i1], [o]):
+            # two beds for both haps; one reference with both haps (combine)
+            if isinstance(bd, cfg.Diploid1BuildData) and hap is None:
+                hap_to_dip_f(i0, i1, o, bd)
+            else:
+                assert False, "this should not happen"
+        case _:
+            assert False, "this should not happen"
 
 
 def main(smk: Any, sconf: cfg.GiabStrats) -> None:
