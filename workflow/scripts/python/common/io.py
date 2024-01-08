d0329b1509a3469f91d326368dc5094b45ebe491
ndwarshuis <ndwar@yavin4.ch>
2024-01-08 15:31:41 -0500

    REF delete dead code

--- workflow/scripts/python/common/io.py
+++ workflow/scripts/python/common/io.py
@@ -1,11 +1,8 @@
 import hashlib
-from typing import TypeVar
 from logging import Logger
 from pathlib import Path
 from Bio import bgzf  # type: ignore
 import gzip
-
-X = TypeVar("X")
 
 
 def get_md5(path: str, unzip: bool = False) -> str:
@@ -26,17 +23,6 @@ def setup_logging(path: str, console: bool = False) -> Logger:
     if console:
         logger.addHandler(logging.StreamHandler())
     return logger
-
-
-def test_not_none(log: Logger, thing: X | None, what: str | None = None) -> X:
-    if thing is None:
-        log.error(
-            "This should not happen: %s is None" % what
-            if what is not None
-            else "object"
-        )
-        exit(1)
-    return thing
 
 
 def is_gzip(p: Path) -> bool:
